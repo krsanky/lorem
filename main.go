df39f24c1813a02a2c36b34a8b3cab8aa751a42a
paul wisehart <paul@oldcode.org>
2017-06-30 18:14:21 -0400

    modify directory/code layout

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
+//    {% lorem %} will output the common “lorem ipsum” paragraph.
+//    {% lorem 3 p %} will output the common “lorem ipsum” paragraph and two random paragraphs each wrapped in HTML <p> tags.
+//    {% lorem 2 w random %} will output two random Latin words.
+
+
 import (
 	"fmt"
+
+	"oldcode.org/web/lorem"
 )
 
 
 func main() {
     fmt.Printf("Main ...\n")
+	lorem.Test()
 }
